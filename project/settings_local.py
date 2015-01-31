5ebf8603aa1ea2b14c5dc4297562479ce8241b94
Dmitriy Butovskiy <d1mskat@gmail.com>
2015-02-01 00:43:35 +0200

    Update the PRODUCTION_HOST

--- project/settings_local.py
+++ project/settings_local.py
@@ -16,4 +16,4 @@ LOCAL_PATH = normpath(join(dirname(__file__), '..'))
 # These are the hostnames as returned by platform.node().
 # If you aren't sure what to put, leave them blank and the error message should tell you which hostname Python sees.
 DEVELOPMENT_HOST = 'vesperalwall860-HP-ENVY-m6-Notebook-PC'
-PRODUCTION_HOST = ''
+PRODUCTION_HOST = 'apache2-python.2.2.31300.but-service'
